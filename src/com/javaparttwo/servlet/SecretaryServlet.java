70c1956ef0cf62824303a7376bccd8167606d8f1
Panos Ioannidis <panos277@hotmail.com>
2018-05-09 16:44:52 +0300

    database config

--- src/com/javaparttwo/servlet/SecretaryServlet.java
+++ src/com/javaparttwo/servlet/SecretaryServlet.java
@@ -1,5 +1,62 @@
 package com.javaparttwo.servlet;
 
-public class SecretaryServlet {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class SecretaryServlet
+ */
+@WebServlet({ "/SecretaryServlet", "/secretary" })
+public class SecretaryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Resource(name="jdbc/javapart2")
+	private DataSource ds;
+
+    /**
+     * Default constructor. 
+     */
+    public SecretaryServlet() {
+    	//
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi from get request!");
+		
+		try {
+			Connection con = ds.getConnection();
+			System.out.println(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.getWriter().append("! Served at : ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
