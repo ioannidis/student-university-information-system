93f1082c0637597aebec507c474672acd7656f6b
Thanos Paravantis <p16112@students.cs.unipi.gr>
2018-05-16 13:48:09 +0300

    Remove unused variable and imports

--- src/com/javaparttwo/servlet/SecretaryServlet.java
+++ src/com/javaparttwo/servlet/SecretaryServlet.java
@@ -1,23 +1,15 @@
 package com.javaparttwo.servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import com.javaparttwo.model.Course;
@@ -47,7 +39,6 @@ public class SecretaryServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		AuthService auth = new AuthService(request, response);
-		HttpSession session = request.getSession();
 		
 		if (!auth.isLoggedIn()) {
 			response.sendRedirect("login");
@@ -66,7 +57,6 @@ public class SecretaryServlet extends HttpServlet {
 		
 		request.setAttribute("courses", courses);
 		request.getRequestDispatcher("WEB-INF/views/secretary/secretary.jsp").forward(request, response);
-
 	}
 
 	/**
