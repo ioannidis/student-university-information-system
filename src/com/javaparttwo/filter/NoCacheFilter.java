4716f84e4944229ae84acd9bd24db2ccf50edeff
Panos Ioannidis <panos277@hotmail.com>
2018-05-22 15:49:57 +0300

    Back button fixed

---
+++ src/com/javaparttwo/filter/NoCacheFilter.java
@@ -0,0 +1,36 @@
+package com.javaparttwo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class NoCacheFilter implements Filter {
+
+	public NoCacheFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+	    HttpServletResponse res = (HttpServletResponse) response;
+
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.setHeader("Pragma", "no-cache");
+        res.setDateHeader("Expires", 0);
+
+        chain.doFilter(request, response);
+
+	}
+
+}
