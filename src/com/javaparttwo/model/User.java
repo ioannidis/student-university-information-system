8ff6c24fb19d17c176dbd6d54d52932cda3d9cdd
Thanos Paravantis <p16112@students.cs.unipi.gr>
2018-05-22 13:46:24 +0300

    Format User class

--- src/com/javaparttwo/model/User.java
+++ src/com/javaparttwo/model/User.java
@@ -9,7 +9,7 @@ public class User {
      * The username representing this user.
      */
     private String username;
-    
+
     /**
      * The password of the user.
      */
@@ -24,12 +24,12 @@ public class User {
      * The last name of this user.
      */
     private String surname;
-    
+
     /**
      * The phone_number of this user.
      */
     private int phoneNumber;
-    
+
     /**
      * The email of this user.
      */
@@ -41,24 +41,44 @@ public class User {
     private String roleId;
 
     /**
-     * The total number of user objects created during the runtime of this application.
+     * The total number of user objects created during the runtime of this
+     * application.
      */
     private static int usersCounter = 0;
 
-    public User(String username, String password, String name, String surname, int phoneNumber, String email, String roleId) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.surname = surname;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.roleId = roleId;
+    /**
+     * Creates a new User.
+     * 
+     * @param username
+     *            The username of the user.
+     * @param password
+     *            The password of the user.
+     * @param name
+     *            The name of the user.
+     * @param surname
+     *            The surname of the user.
+     * @param phoneNumber
+     *            The phone number of the user.
+     * @param email
+     *            The email of the user.
+     * @param roleId
+     *            The role of the user.
+     */
+    public User(String username, String password, String name, String surname, int phoneNumber, String email,
+	    String roleId) {
+	this.username = username;
+	this.password = password;
+	this.name = name;
+	this.surname = surname;
+	this.phoneNumber = phoneNumber;
+	this.email = email;
+	this.roleId = roleId;
 
-        System.out.println("Creating new user. Total: " + ++usersCounter);
+	System.out.println("Creating new user. Total: " + ++usersCounter);
 
-        // Display creation message on.
-        if (usersCounter == 1)
-            System.out.println(this + System.lineSeparator());
+	// Display creation message on.
+	if (usersCounter == 1)
+	    System.out.println(this + System.lineSeparator());
     }
 
     /**
@@ -67,34 +87,36 @@ public class User {
      * @return The username representing this user.
      */
     public String getUsername() {
-        return username;
+	return username;
     }
 
     /**
      * Set the username of this user.
      *
-     * @param username The new username.
+     * @param username
+     *            The new username.
      */
     public void setUsername(String username) {
-        this.username = username;
+	this.username = username;
     }
-    
+
     /**
      * Get the password of the user.
      *
      * @return The password of the user.
      */
     public String getPassword() {
-        return password;
+	return password;
     }
-    
+
     /**
      * Set the password of this user.
      *
-     * @param password The new password.
+     * @param password
+     *            The new password.
      */
     public void setPassword(String password) {
-        this.password = password;
+	this.password = password;
     }
 
     /**
@@ -103,16 +125,17 @@ public class User {
      * @return The first name of this user.
      */
     public String getName() {
-        return name;
+	return name;
     }
 
     /**
      * Set the first name of this user.
      *
-     * @param name The new first name.
+     * @param name
+     *            The new first name.
      */
     public void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
 
     /**
@@ -121,69 +144,85 @@ public class User {
      * @return The last name of this user.
      */
     public String getSurname() {
-        return surname;
+	return surname;
     }
 
     /**
      * Set the last name of this user.
      *
-     * @param surname The new last name.
+     * @param surname
+     *            The new last name.
      */
     public void setSurname(String surname) {
-        this.surname = surname;
+	this.surname = surname;
     }
-    
+
     /**
      * Get the last name of this user.
      *
      * @return The last name of this user.
      */
     public int getPhoneNumber() {
-        return phoneNumber;
+	return phoneNumber;
     }
 
     /**
      * Set the last name of this user.
      *
-     * @param surname The new last name.
+     * @param surname
+     *            The new last name.
      */
     public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
+	this.phoneNumber = phoneNumber;
     }
-    
+
     /**
      * Get the last name of this user.
      *
      * @return The last name of this user.
      */
     public String getEmail() {
-        return email;
+	return email;
     }
 
     /**
      * Set the last name of this user.
      *
-     * @param surname The new last name.
+     * @param surname
+     *            The new last name.
      */
     public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(String roleId) {
-        this.roleId = roleId;
+	this.email = email;
     }
 
     /**
-     * Get the total number of user objects created during the runtime of this application.
+     * Get the role of the user.
+     * 
+     * @return The role of the user.
+     */
+    public String getRoleId() {
+	return roleId;
+    }
+
+    /**
+     * Set the role of the user.
+     * 
+     * @param roleId
+     *            The new role of the user.
+     */
+    public void setRoleId(String roleId) {
+	this.roleId = roleId;
+    }
+
+    /**
+     * Get the total number of user objects created during the runtime of this
+     * application.
      *
-     * @return The total number of user objects created during the runtime of this application.
+     * @return The total number of user objects created during the runtime of this
+     *         application.
      */
     public static int getUsersCounter() {
-        return usersCounter;
+	return usersCounter;
     }
 
     /**
@@ -192,6 +231,7 @@ public class User {
      * @return Returns a string containing all properties of this user.
      */
     public String toString() {
-        return "[User] username: " + username + ", password: ****, name: " + name + ", surname: " + surname + ", phone number: " + phoneNumber + " email: " + email + "  role id: " + roleId;
+	return "[User] username: " + username + ", password: ****, name: " + name + ", surname: " + surname
+		+ ", phone number: " + phoneNumber + " email: " + email + "  role id: " + roleId;
     }
 }
