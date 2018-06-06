3591dabcb78544ebd5975becc4d67e7f2d7831b0
Panos Ioannidis <panos277@hotmail.com>
2018-06-06 20:08:51 +0300

    custom ccs js updated

--- WebContent/assets/js/custom.js
+++ WebContent/assets/js/custom.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $('#dataTable-list').DataTable({
     	"iDisplayLength": 25
     });
+    $('#dataTable').DataTable({
+    	"iDisplayLength": 25
+    });
     $('table.table-semester').DataTable({
     	"paging": false
     });
