776221cd071f5768a7f080f61b44d44bafa50603
Panos Ioannidis <panos277@hotmail.com>
2018-06-09 15:20:08 +0300

    used only for course listing

--- src/com/javaparttwo/servlet/CourseServlet.java
+++ src/com/javaparttwo/servlet/CourseServlet.java
@@ -45,69 +45,10 @@ public class CourseServlet extends HttpServlet {
 		}
 	
 		CourseService courseService = new CourseService(ds);
-		String id = getParameterOrDefault(request, "id", "");
-		String action = getParameterOrDefault(request, "action", "all");
-	
-		switch (action) {
-			case "create": {
-				if (!authService.hasRole("secretary")) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN);
-					return;
-			    }
-				
-				ProfessorService professorService = new ProfessorService(ds);
-				
-				request.setAttribute("instructors", professorService.getProfessors());
-				request.getRequestDispatcher("WEB-INF/views/course/create.jsp").forward(request, response);
-			    break;
-			}
-			case "show": {
-			    if (!authService.hasRole("secretary")) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN);
-					return;
-			    }
-		
-			    Course course = courseService.getCourse(id);
-			    request.setAttribute("course", course);
-			    request.getRequestDispatcher("WEB-INF/views/course/show.jsp").forward(request, response);
-			    break;
-			}
-			case "edit": {
-			    if (!authService.hasRole("secretary")) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN);
-					return;
-			    }
-		
-			    ProfessorService professorService = new ProfessorService(ds);
-			    Course course = courseService.getCourse(id);
-		
-			    request.setAttribute("course", course);
-			    request.setAttribute("instructors", professorService.getProfessorsByDept(authService.getUser().getDepartmentId()));
-			    request.getRequestDispatcher("WEB-INF/views/course/edit.jsp").forward(request, response);
-			    break;
-			}
-			default: {
-			    List<Course> courses = courseService.getCourses(authService.getUser().getDepartmentId());
-			    request.setAttribute("courses", courses);
-			    request.getRequestDispatcher("WEB-INF/views/course/index.jsp").forward(request, response);
-			    break;
-			}
-		}
-    }
 
-    /**
-     * Returns a default value if the parameter on the request is null.
-     * 
-     * @param request
-     *            The current request.
-     * @param name
-     *            The name of the parameter to fetch.
-     * @param def
-     *            The default value to return if the parameter is null.
-     * @return A default value or the parameter.
-     */
-    private String getParameterOrDefault(HttpServletRequest request, String name, String def) {
-	return request.getParameter(name) == null ? def : request.getParameter(name);
+		List<Course> courses = courseService.getCourses(authService.getUser().getDepartmentId());
+	    request.setAttribute("courses", courses);
+	    request.getRequestDispatcher("WEB-INF/views/course/index.jsp").forward(request, response);
     }
 
     /**
@@ -115,58 +56,7 @@ public class CourseServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-    	AuthService authService = new AuthService(request.getSession());
-		if (!authService.isLoggedIn()) {
-		    response.sendRedirect("login");
-		    return;
-		}
-		
-		CourseService courseService = new CourseService(ds);
-		String id = getParameterOrDefault(request, "id", "");
-		String action = getParameterOrDefault(request, "action", "");
-
-		switch (action) {
-			case "delete": {
-			    if (!authService.hasRole("secretary")) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN);
-					return;
-			    }
-		
-			    courseService.deleteCourse(id);
-			    response.sendRedirect("secretary");
-			    break;
-			}
-			case "save": {
-				if (!authService.hasRole("secretary")) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN);
-					return;
-			    }
-				
-				courseService.addCourse(request.getParameter("id"),request.getParameter("title"),request.getParameter("semester"),
-						request.getParameter("ects"),request.getParameter("teachingHours"),request.getParameter("teachingInstructor"), authService.getUser().getDepartmentId());
-				
-				response.sendRedirect("secretary");
-			    break;
-			}
-			case "update": {
-				if (!authService.hasRole("secretary")) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN);
-					return;
-			    }
-				
-			    Course course = courseService.getCourse(id);
-			    courseService.editCourse(id, getParameterOrDefault(request, "title", course.getTitle()),
-				    getParameterOrDefault(request, "ects", Integer.toString(course.getEcts())),
-				    getParameterOrDefault(request, "teachingHours", Integer.toString(course.getTeachingHours())),
-				    getParameterOrDefault(request, "teachingInstructor", course.getInstructorUsername()));
-			    response.sendRedirect("secretary");
-			    break;
-			}
-			default: {
-			    doGet(request, response);
-			    break;
-			}
-		}
+    	doGet(request, response);		
     }
     
 }
